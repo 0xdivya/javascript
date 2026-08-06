6266b8eaeb5b44622d622c0fe381124b77bcd25d
VivekSotra <vivek.sotra8@gmail.com>
2026-08-06 15:24:10 +0530

    Added javascript variables notes

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+// Variable declarations in JavaScript
+// Using const for a variable that won't change
+const accountId = 147852
+//accountId = 258963 // This will throw an error because accountId is a constant
+
+let accountEmail = "divya@example.com"
+var accountPassword = "securePassword123"
+accountCity = "New York"
+
+accountEmail = "newemail@example.com"
+accountPassword = "new123456"
+accountCity= "Los Angeles"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
